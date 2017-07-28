70ec9aa4db1b5c703fcfb0ebc1bf11b08d7f80c9
Phillip Hall <phillip.hall@leap.com.au>
2017-07-28 11:21:51 +1000

    Format files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'),
-  mongoose = require('mongoose'),
-  request = require('request'),
-  winston = require('winston');
+    mongoose = require('mongoose'),
+    request = require('request'),
+    winston = require('winston');
 
 mongoose.Promise = Promise;
 
@@ -9,18 +9,20 @@ winston.level = 'debug';
 
 //DB setup
 winston.debug(`Connecting to "${process.env.DB_NAME}"`);
-mongoose.connect(`mongodb://${process.env.DB_NAME}:27017`, {useMongoClient: true})
-  .then((db) => {
-    winston.info('Mongoose OK');
-  })
-  .catch((err) => {
-    winston.error(err);
-  });
+mongoose.connect(`mongodb://${process.env.DB_NAME}:27017`, {
+        useMongoClient: true
+    })
+    .then((db) => {
+        winston.info('Mongoose OK');
+    })
+    .catch((err) => {
+        winston.error(err);
+    });
 
 const app = express();
 
-app.get('/', function(req, res){
-  res.send("Hello World");
+app.get('/', function(req, res) {
+    res.send("Hello World");
 });
 
 app.get('/classes', function(req, res) {
@@ -31,6 +33,6 @@ app.get('/image', function(req, res) {
     request.get('https://hub.docker.com/public/images/logos/mini-logo.svg').pipe(res);
 });
 
-app.listen(3000, function(){
-  winston.debug('Example app listening on port 3000!');
+app.listen(3000, function() {
+    winston.debug('Example app listening on port 3000!');
 });
